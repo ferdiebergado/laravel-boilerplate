575fa7bfb9decf82ddfcf19e06cbd7343344de7f
ferdie <ferdiebergado@gmail.com>
2018-07-29 15:13:11 +0800

    Added change password feature

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -8,47 +8,60 @@ use App\Policies\UsersPolicy;
 class UserRequest extends FormRequest
 {
     /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
+    * Determine if the user is authorized to make this request.
+    *
+    * @return bool
+    */
     public function authorize()
     {
         return new UsersPolicy;
     }
-
+    
     /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array
-     */
+    * Get the validation rules that apply to the request.
+    *
+    * @return array
+    */
     public function rules()
     {
         switch ($this->method()) {
             case 'GET':
             case 'DELETE':
-                {
-                    return [];
-                }
+            {
+                return [];
+            }
             case 'POST':
-                {
-                    return [
-                        'name' => 'required|string|max:150',
-                        'email' => 'required|string|max:150|email|unique:users,email',
-                        'password' => 'required|confirmed|string|max:150'
-                    ];
-                }
+            {
+                return [
+                    'name' => 'required|string|max:150',
+                    'email' => 'required|string|max:150|email|unique:users,email',
+                    'password' => 'required|confirmed|string|max:150'
+                ];
+            }
             case 'PUT':
             case 'PATCH':
-                {
-                    return [
-                        'name' => 'required|string|max:150',
-                        'email' => 'required|string|max:150|email|unique:users,email,' . $this->route()->parameters['id'],
-                        'password' => 'confirmed|string|max:150|nullable'
-                    ];
-                }
+            {
+                return [
+                    'name' => 'required|string|max:150',
+                    'email' => 'required|string|max:150|email|unique:users,email,' . $this->route()->parameters['id'],
+                    'old_password' => [
+                        'nullable',
+                        'string',
+                        'max:150',
+                        'required_with:password',
+                        function ($attribute, $value, $fail) {
+                            if (!empty($value)) {
+                                if (!\Illuminate\Support\Facades\Hash::check($value, \App\User::findOrFail($this->route()->parameters['id'])->password)) {
+                                    return $fail('Password does not match.');
+                                }
+                            }
+                        },
+                    ],
+                    'password' => 'sometimes|required_with:old_password|confirmed|string|max:150|nullable'
+                ];
+            }
             default:
-                break;
+            break;
         }
     }
 }
